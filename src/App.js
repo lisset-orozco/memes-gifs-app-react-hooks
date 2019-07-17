6631a73e0b582729c34a3244beedb6e90c76fda8
lisset-orozco <liss612@gmail.com>
2019-07-17 14:34:03 -0500

    Define structure and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Nav from "./components/Nav";
+import Search from "./components/Search";
 
 const App = () => (
   <div>
     <Nav />
+    <Search />
   </div>
 );
 
---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles/Search.css";
+
+export default function() {
+  return (
+    <section className="search">
+      <input
+        id="searching"
+        placeholder="Search for Memes you love"
+        type="text"
+      />
+      <img
+        src="https://blog.airtable.com/content/images/2018/04/blockIconImages-2F3zZbh6x3QMSH6WqBjMGH_search-icon.png"
+        alt="search"
+      />
+    </section>
+  );
+}

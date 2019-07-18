1199d809eeb33a496ec14b1c0153570c83488f4a
lisset-orozco <liss612@gmail.com>
2019-07-18 15:14:43 -0500

    Define structure and styles

---
+++ src/components/Results.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./styles/Results.css";
+
+export default function() {
+  return (
+    <section id="results" className="results">
+      <figure>
+        <img
+          src="https://media0.giphy.com/media/9JBKdsyIL61WM/200w.webp?cid=ecf05e475c66f1ff6c7a33746b5dc70a"
+          alt="img1"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://media.giphy.com/media/GdNbYAcBjfqTK/giphy.gif"
+          alt="img2"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://media0.giphy.com/media/9JBKdsyIL61WM/200w.webp?cid=ecf05e475c66f1ff6c7a33746b5dc70a"
+          alt="img3"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://media0.giphy.com/media/9JBKdsyIL61WM/200w.webp?cid=ecf05e475c66f1ff6c7a33746b5dc70a"
+          alt="img5"
+        />
+      </figure>
+    </section>
+  );
+}
